e179a00abfbc04820ecf333569223eaf7ee88f1e
Marko Pavic <markopavicCsharp@gmail.com>
2019-09-23 19:45:06 +0200

    off dugme i procenat

--- Racunar/MainWindow.xaml.cs
+++ Racunar/MainWindow.xaml.cs
@@ -27,87 +27,101 @@ namespace Racunar
     
     public partial class MainWindow : Window
     {
+        
         private double firstNum,secnum,infinity,result;
 
-        Operatori SelektovanOperater = Operatori.Ne_Selektovan;
+            // za deselektovanje operatora (operacije)
+            Operatori SelektovanOperater = Operatori.Ne_Selektovan;
 
+        // proverava da li je upisano nesto posle selektovanja operatora
         bool IsOcupied = true,
-            check = true, afterJednko = true,
-            afterDot = false;
 
-        Button NumbersButtons;
+        // proverava da li firstnum ima upisanu vrednost
+        CheckForFirstNum = true,
+
+        // proverava da li je vec pritisnut taster jednako
+        afterJednko = true,
+
+        //proverava da li je tacka stavljena
+        afterDot = false,
+
+        operationPerformed = false,
+        operationPerformed1 = false;
+            
+            // za uzimanje vrednosti iz sendera 
+            Button NumbersButtons;
 
         public MainWindow()
         {
             
             InitializeComponent();
         }
+        // event za brojeve
 
         private void Numbers_Click (object sender , RoutedEventArgs e)
         {
-            if (afterJednko && !afterDot)
-            {
-                PocetneVrednostiZaOperatore();
-                SelektovanOperater = Operatori.Ne_Selektovan;
-
-            }
+            //posle pritiska tastera jednako  i ako nije pritisnut zarez stavlja sve na pocetne vrednosti
+            // deselektuje operaciju
+            if (operationPerformed) { }
+             else if (afterJednko && !afterDot)
+                {
+                    PocetneVrednostiZaOperatore();
+                    SelektovanOperater = Operatori.Ne_Selektovan;
+                }
+            // 
             afterDot = false;
             afterJednko = false;
 
-
+            // setuje font labela u odnostu na broj cifara
             SetLabelFontSize(ResultLabel);
-            check = true;
 
+            //setuje da je nesto upisano za prvi br
+            CheckForFirstNum = true;
+
+            // posle greske i kucanja br enejbluje dugmice ponovo
             if (Plus.IsEnabled == false)
             {
                 OmoguciDugmicePoslegreske(); 
             }
+
+            // cekuje da li je bilo greske i ako je bilo vraca font... na pocetne vrednosti
             if (ResultLabel.Foreground == Brushes.Red)
             {
                PocetneVrednostiZaOperatore();
             }
-            
+
+            // ukoliko je nesto upisano brise reultLabel za kucanje drugog broja
             if (IsOcupied)
             {
                 ResultLabel.Content = "";
             }
             IsOcupied = false;
 
+            // kastujemo sender objekat u dugme 
             NumbersButtons = (Button)sender;
 
-          /*  if (sender == Jedan) {
-                
-                selectedItem = 1; }
-            if (sender == Dva) selectedItem = 2;
-            if (sender == Tri) selectedItem = 3;
-            if (sender == Cetri) selectedItem = 4;
-            if (sender == Pet) selectedItem = 5;
-            if (sender == Sest) selectedItem = 6;
-            if (sender == Sedam) selectedItem = 7;
-            if (sender == Osam) selectedItem = 8;
-            if (sender == Devet) selectedItem = 9;
-            if (sender == Nula) selectedItem = 0;*/
-
+            //ako je nula na ekranu brisemo je i upisujemo br
             if (ResultLabel.Content.ToString() == "0")
             {
                 ResultLabel.Content = $"{NumbersButtons.Content}";
             }
+            //ako nije dodajemo br na postojeci
             else if(ResultLabel.Content.ToString().Length<=15)
             {
-                
                 ResultLabel.Content = $"{ResultLabel.Content}{NumbersButtons.Content}";
-                
             }
         }
-
+        // ac vraca na pocetne vrednosti
         private void AcButton_Click(object sender, RoutedEventArgs e)
         {
             OmoguciDugmicePoslegreske();
             PocetneVrednostiZaOperatore();
             ResultLabel.Content = "0";
+            operationPerformed = false;
+            operationPerformed1 = false;
 
         }
-
+        // mennja znak mnozenjem sa -1
         private void PlusMinus_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(ResultLabel.Content.ToString(),out firstNum))
@@ -118,6 +132,7 @@ namespace Racunar
 
         private void Tacka_Click(object sender, RoutedEventArgs e)
         {
+            //proverava da li je vec zarez u labelu
             if (!afterJednko)
             {
                 if (!ResultLabel.Content.ToString().Contains(","))
@@ -125,6 +140,7 @@ namespace Racunar
                     ResultLabel.Content = $"{ResultLabel.Content},";
                 }
             }
+            //ako je pritisnu samo zarez posle jednako dodaje nulu ispred zareza
             else
             {
                 PocetneVrednostiZaOperatore();
@@ -136,21 +152,30 @@ namespace Racunar
                 }
             }
         }
-
+        // nisam zavrsio
         private void Procenat_Click(object sender, RoutedEventArgs e)
         {
-            if(double.TryParse(ResultLabel.Content.ToString(),out firstNum))
+            if (double.TryParse(ResultLabel.Content.ToString(), out secnum))
             {
-                ResultLabel.Content = firstNum / 100;
+                secnum = secnum / 100;
+                ResultLabel.Content = secnum;
             }
+            
         }
 
         private void Operacije_Click(object sender,RoutedEventArgs e)
         {
             afterJednko = false;
-            check = true;
+            CheckForFirstNum = true;
+            if (operationPerformed)
+            {
+                Jednako_Click(sender, e);
+
+            }
+
+            //puni firstnum sa vrednoscu
             if (double.TryParse(ResultLabel.Content.ToString(), out firstNum))
-                
+              //setuje operaciju i ispisuje prvi broj u gornji label
              if (sender == Podeljeno)
              {
                  OperatorLabel.Content = "/";
@@ -180,21 +205,30 @@ namespace Racunar
                 FirstNUmLabel.Content = firstNum;
                 
             }
-            
-            IsOcupied = true;
+
            
+
+            IsOcupied = true;
+            operationPerformed = true;
             
         }
 
+        
+
         private void Jednako_Click(object sender, RoutedEventArgs e)
         {
-
-            if (check && firstNum != 0)
+            //proverava da li je prvi br im vrednost ako ima puni secnum
+            if (CheckForFirstNum && firstNum != 0)
+            {
+                double.TryParse(ResultLabel.Content.ToString(), out secnum);
+            }
+            else if (firstNum == 0 && SelektovanOperater != Operatori.Ne_Selektovan )
             {
                 double.TryParse(ResultLabel.Content.ToString(), out secnum);
             }
 
-
+            //deljenje sa nulom, ima exeption ali nisam ga upotrebio
+            // izmenicu to kasnije
             if ((SelektovanOperater == Operatori.Deljenje && secnum == 0))
             {
                 OnemoguciDugmicePoslegreske();
@@ -203,6 +237,9 @@ namespace Racunar
                 ResultLabel.FontSize = 6;
                 ResultLabel.Content = "ERROR DEVIDE BY ZERO NOT DEFINED  ";
             }
+            
+            //proverava sta je odabrano od operacije i izvrsava ispisuje resultat
+            //imao sam problem sa countom i length  pa sam rezultat ubacio u double pa u label content
             else if (SelektovanOperater == Operatori.Deljenje ||
                 SelektovanOperater == Operatori.Mnozenje||
                 SelektovanOperater == Operatori.oduzimanje||
@@ -239,9 +276,10 @@ namespace Racunar
 
                 
             }
-            if (result != 0)
+            //ako result nije nulla, nije selektovan operator result je 0
+            if (result != 0 && !ResultLabel.Content.ToString().Contains("ERROR"))
             {
-                ResultLabel.Content = result.ToString();
+                ResultLabel.Content = result.ToString("R");
             }
             
             
@@ -250,7 +288,7 @@ namespace Racunar
             
             
             afterJednko = true;
-
+            //out of range result , ne znam da li ima exeption bas za ovaj slucaj moram da proverim 
             if (double.TryParse(ResultLabel.Content.ToString(), out infinity))
             {
                 if (double.IsPositiveInfinity(infinity) || double.IsNegativeInfinity(infinity))
@@ -264,13 +302,15 @@ namespace Racunar
 
             OperatorLabel.Content = "";
             FirstNUmLabel.Content = "";
+            //nakon jednako ako secnum nije nula puni first num vrednoscu ispisanom na ekranu 
             if (secnum != 0)
             {
                 firstNum = double.Parse(ResultLabel.Content.ToString());
             }
 
-            check = false;
-
+            CheckForFirstNum = false;
+            operationPerformed = false;
+           
 
         }
 
@@ -283,7 +323,7 @@ namespace Racunar
             Mnozenje,
             Ne_Selektovan
         }
-
+        // pomeranje windowsa
         private void MoveWindowsMouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -293,7 +333,7 @@ namespace Racunar
         }
 
         
-
+        //close
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
@@ -388,18 +428,13 @@ namespace Racunar
             }
         }
 
-      /*  public void HundredSeparator(Label label)
+        /*public void HundredSeparator(Label label)
         {
-
-
-            if (!label.Content.ToString().Contains(",")&& !label.Content.ToString().Contains("ERROR"))
+            if (!label.Content.ToString().Contains("ERROR"))
             {
                 double result = Convert.ToDouble(label.Content);
-                label.Content = result.ToString("N0");
+                label.Content = result.ToString("R");
             }
-            
-                
-            
         }*/
     }
 
